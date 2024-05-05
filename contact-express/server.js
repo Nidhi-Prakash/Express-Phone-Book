c26fe998e84cfdd0bac61fec10d6145ec54cb337
Nidhi Prakash <np562019@gmail.com>
2024-05-05 19:34:06 +0530

    added CORS

--- contact-express/server.js
+++ contact-express/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import { userRouter } from "./routes/userRouter.js";
 import { createDbConnection } from "./config/DbConnection.js";
 import { contactsRouter } from "./routes/contactsRouter.js";
@@ -8,14 +8,23 @@ const app = express();
 
 const PORT = 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from all origins
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/contacts", contactsRouter);
 
 app.use(express.static("../phone-book/build"));
-app.get('*', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
+app.get("*", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
 const runServer = async () => {
